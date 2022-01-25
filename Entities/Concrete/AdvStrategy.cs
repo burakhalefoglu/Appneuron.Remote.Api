3628d33fd1df2d01ec86b9bb7aacc8e88d03a731
appneuronco <76439337+appneuronco@users.noreply.github.com>
2022-01-25 18:20:17 +0300

    Update AdvStrategy.cs

--- Entities/Concrete/AdvStrategy.cs
+++ Entities/Concrete/AdvStrategy.cs
@@ -1,6 +1,6 @@
 ﻿namespace Entities.Concrete
 {
-    public class AdvStrategy
+    public class AdvStrategy : DocumentDbEntity
     {
         public string Name { get; set; }
         public float Count { get; set; }
